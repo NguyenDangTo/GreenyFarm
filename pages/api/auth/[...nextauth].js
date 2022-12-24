6c631db824f43c743de810d7df421c5fe3dd7104
dangto2003 <nguyendangto2502@gmail.com>
2022-12-24 13:32:44 +0700

    fix no secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,20 +1,20 @@
-import bcryptjs from 'bcryptjs';
-import NextAuth from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import User from '../../../models/User';
-import db from '../../../utils/db';
+import bcryptjs from "bcryptjs";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "../../../models/User";
+import db from "../../../utils/db";
 
 export default NextAuth({
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({token, user}) {
       if (user?._id) token._id = user._id;
       if (user?.isAdmin) token.isAdmin = user.isAdmin;
       return token;
     },
-    async session({ session, token }) {
+    async session({session, token}) {
       if (token?._id) session.user._id = token._id;
       if (token?.isAdmin) session.user.isAdmin = token.isAdmin;
       return session;
@@ -33,12 +33,13 @@ export default NextAuth({
             _id: user._id,
             name: user.name,
             email: user.email,
-            image: 'f',
+            image: "f",
             isAdmin: user.isAdmin,
           };
         }
-        throw new Error('Invalid email or password');
+        throw new Error("Invalid email or password");
       },
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
